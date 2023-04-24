ccc67de6b67b6dda04622f5436105fa885922626
JeonghoPark <pjhpjh0805@naver.com>
2023-04-25 04:43:51 +0900

    first commit

---
+++ src/main/java/com/example/project_cicd/ProjectCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCicdApplication.class, args);
+	}
+
+}
