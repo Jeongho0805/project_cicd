f84e5f39482159f923c4115d52ff92b9565f56cb
JeonghoPark <pjhpjh0805@naver.com>
2023-04-25 17:54:41 +0900

    test commit

--- src/main/java/com/example/project_cicd/controller/TestController.java
+++ src/main/java/com/example/project_cicd/controller/TestController.java
@@ -1,9 +1,15 @@
 package com.example.project_cicd.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class TestController {
+
+    @GetMapping
+    public String test() {
+        return "cicd 성공";
+    }
 }
