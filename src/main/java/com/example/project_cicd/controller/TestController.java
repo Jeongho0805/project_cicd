82a3e04df5c1623d858cc639ecce433de21865b9
JeonghoPark <pjhpjh0805@naver.com>
2023-05-11 01:42:08 +0900

    commit

--- src/main/java/com/example/project_cicd/controller/TestController.java
+++ src/main/java/com/example/project_cicd/controller/TestController.java
@@ -1,20 +1,24 @@
 package com.example.project_cicd.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class TestController {
 
     @GetMapping
     public String test() {
+        log.info("requestUri => api");
         return "나 성공한거야? ㅜㅜ";
     }
 
-    @GetMapping("jeongho")
+    @GetMapping("/jeongho")
     public String test2() {
+        log.info("requestUri => api/jeongho");
         return "경로 계층 구조 매칭 성공";
     }
 }
