e0ba08cd65168bba5b3076ca060c0ef431131be7
JeonghoPark <pjhpjh0805@naver.com>
2023-05-12 17:02:01 +0900

    commit

--- src/main/java/com/example/project_cicd/controller/TestController.java
+++ src/main/java/com/example/project_cicd/controller/TestController.java
@@ -1,9 +1,12 @@
 package com.example.project_cicd.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigInteger;
 
 @Slf4j
 @RestController
@@ -13,7 +16,7 @@ public class TestController {
     @GetMapping
     public String test() {
         log.info("requestUri => api");
-        return "나 성공한거야? ㅜㅜ";
+        return "성공!";
     }
 
     @GetMapping("/jeongho")
